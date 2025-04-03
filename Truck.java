83e828a6c32a51fc902af8cf5fc7940e861a3b9d
peterParker79 <trypeterparker@gmail.com>
2025-04-03 15:11:14 +0200

    finish point 5

--- Truck.java
+++ Truck.java
@@ -1,2 +1,17 @@
 public class Truck extends Car{
+
+    private double towingCapacity;
+
+    public Truck(String vinNumber, String make, String model, int mileage, double towingCapacity) {
+        super(vinNumber, make, model, mileage);
+        setTowingCapacity(towingCapacity);
+    }
+
+    public double getTowingCapacity() {
+        return towingCapacity;
+    }
+
+    public void setTowingCapacity(double towingCapacity) {
+        this.towingCapacity = towingCapacity;
+    }
 }
