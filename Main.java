7a233e91deb864ccad67d4fe913ecb7c37dd1b72
peterParker79 <trypeterparker@gmail.com>
2025-04-03 16:00:08 +0200

    No complete still

--- Main.java
+++ Main.java
@@ -3,18 +3,22 @@ import java.math.RoundingMode;
 
 public class Main {
 
-    public  double bigDecimalToDouble (BigDecimal bigDecimal){
-        return bigDecimal.setScale(2).doubleValue();
+    public static double bigDecimalToDouble(BigDecimal bigDecimal){
+        return bigDecimal.setScale(2, RoundingMode.HALF_UP).negate().doubleValue();
     }
 
     public  double reverseBigDecimal (BigDecimal bigDecimal){
-        return bigDecimal.setScale(2, RoundingMode.HALF_UP).negate().doubleValue();
+
+        return bigDecimal.setScale(2, RoundingMode.HALF_UP).doubleValue();
     }
 
 
     public static void main(String[] args) {
 
-
+        BigDecimal bigDecimal = new BigDecimal("35.7656");
+        double result = bigDecimalToDouble (bigDecimal);
+        //System.out.printf("Result bigDecimalToDouble method: %f", result);
+        System.out.println( result);
     }
 
 
